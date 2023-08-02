f87f000434c2553b34d28ae125a284351983a022
DafGet <tenbb007@hotmail.com>
2023-08-02 16:40:25 -0600

    Initial Commit

---
+++ Linterna/LinternaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinternaApp.swift
+//  Linterna
+//
+//  Created by DAVID BT on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct LinternaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
